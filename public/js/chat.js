81bbc665dfb1f9bb4507106d0bb038804e056130
Karanbir Singh <31749546+KaranbirSingh7@users.noreply.github.com>
2017-11-20 16:00:52 -0500

    Add Join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,23 @@
 //Start connection
 var socket = io();
 
+//AutoScroll
+function autoscroll() {
+	//Selectors
+	var messages = jQuery('#message-list');
+	var newMessage = messages.children('li:last-child');
+	//Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight(); 
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		console.log('Should Scroll');
+	}
+};
+
 socket.on('connect', function () {
 	console.log('Connected to server');
 });
